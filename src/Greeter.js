205616541668fa1c65ca60ed1957efa6c8fa0a9c
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-21 16:05:12 -0700

    example of React way of handling events, onClick in this case

--- src/Greeter.js
+++ src/Greeter.js
@@ -3,10 +3,16 @@ import React from 'react';
 // You should not alter value of props in a component.
 // Props should be treated as immutable
 function Greeter({name, excitement = 1, age}) {
+
+    const greet = () => {
+        alert(`HELLO THERE, ${name}`)
+    }
+
     return (
     <>
         <h1>Hi there, your {name}{"!".repeat(excitement)}</h1>
         <p>You are {age} years old.</p>
+        <button onClick={greet}>Click Me</button>
     </>
     )
 }
