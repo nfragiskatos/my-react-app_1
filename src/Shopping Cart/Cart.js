9813877601f74968a40b3f57aada42f305286048
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-21 21:05:06 -0700

    example of using useEffect, which is a higher order function that gets called everytime the component changes, and you pass 2 arguments to. 1. A callback function. What useEffect calls when it is called. 2. A dependency array. A list of pieces of state that we want to monitor for changes. If any of the pieces of state change, then the callback (first argument) gets ran.

--- src/Shopping Cart/Cart.js
+++ src/Shopping Cart/Cart.js
@@ -1,10 +1,25 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import CartItem from './CartItem';
 import './Cart.css';
 
 function Cart({initialItems}) {
 
-    const [items, setItems] = useState(initialItems)
+    const initialState = JSON.parse(window.localStorage.getItem('items'));
+    const [items, setItems] = useState(initialState || initialItems)
+    const [count, setCount] = useState(1);
+
+    // Gets called everytime component is rendered. Takes 2 arguments.
+    // 1. Callback function
+    // 2. Dependency array - a list of pieces of state that we want to monitor for changes.
+    //      if any of the pieces of state changes, then the callback gets ran.
+    useEffect(() => {
+        window.localStorage.setItem('items', JSON.stringify(items))
+    }, [items]);
+
+    useEffect(() => {
+        console.log("Count EFFECT!");
+    }, [count]);
+
     const updateQty = (id, newQty) => {
         const newItems = items.map(item => {
             if (item.id === id) {
@@ -20,6 +35,7 @@ function Cart({initialItems}) {
     ), 0).toFixed(2)
     return (
         <div className="Cart">
+            <button onClick={() => setCount(count + 1)}>{count}</button>
             <h1 className="Cart-title">Shopping Cart</h1>
             <div className="Cart-items">
                 {items.map(item => (
