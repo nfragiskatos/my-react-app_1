4e81ef7d026c8c22a569ab719d717669046d7213
Nicholas Fragiskatos <nfragiskatos@outlook.com>
2021-04-21 20:27:31 -0700

    example of how to update state from a parent component in a child component. Typical convention is to create an update function in the parent component, and this function typically takes an id argument and an argument for whatever piece of data we want to update. Then pass that update function down to the child as part of props. When the child component needs to update the state it can just call the update function with the appropriate parameters.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,21 @@ import Navbar from './Navbar';
 import Greeter from './Greeter';
 import Counter from './Counter'
 import MoodToggler from './MoodToggler'
+import Cart from './Shopping Cart/Cart'
 import './App.css';
+
+const items = [
+  {id: 1, name: 'Taco Seasoning', price: 2.25, qty: 2},
+  {id: 2, name: 'Pinto Beans', price: 1.99, qty: 3},
+  {id: 3, name: 'Sour Cream', price: 3.50, qty: 1}
+]
 
 // Pass any other value besides string in proprs (boolean, object, number, etc.)
 // it needs to be wrapped in {}
 function App() {
   return (
     <div>
+      <Cart initialItems={items}/>
       <Counter step={5}/>
       <Counter/>
       <MoodToggler/>
---
+++ src/Shopping Cart/Cart.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import CartItem from './CartItem'
+
+function Cart({initialItems}) {
+
+    const [items, setItems] = useState(initialItems)
+    const updateQty = (id, newQty) => {
+        const newItems = items.map(item => {
+            if (item.id === id) {
+                return {...item, qty: newQty}
+            }
+            return item;
+        })
+        setItems(newItems);
+    }
+
+    const grandTotal = items.reduce((total, item) => (
+        total + item.qty * item.price
+    ), 0).toFixed(2)
+    return (
+        <div className="Cart">
+            <h1>I am cart</h1>
+            <div className="Cart-items">
+                {items.map(item => (
+                    <CartItem key={item.id} updateQty={updateQty} {...item}/>
+                ))}
+            </div>
+            <h2>
+                Grand Total: ${grandTotal}
+            </h2>
+        </div>
+    )
+}
+
+export default Cart;
